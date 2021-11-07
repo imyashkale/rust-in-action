ce00e1c09a7d4865a93f8c155d6f17b94ff85589
Yash Kale <yashkale46@gmail.com>
2021-11-07 09:39:42 +0530

    grep lite

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,127 @@
-use std::rc::Rc;
-use std::sync::{Arc,Mutex };
+use clap::{App, Arg};
+use regex::Regex;
+
+// files
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
-    // Executing the function.
-    greet_world();
-    process_csv();
-    create_integer()
+    read_from_file()
 }
 
-fn create_integer(){
-    // 1. safety first priority
-    // 2. data is immutable by default
-    // 3. compile time checks strongly preferred
-    // 4. safety should be "Zero-cost abstraction"
-    let a = 10;
-    println!("Stack-Integer : {:?}",a );
+pub fn read_from_file(){
+    let f = File::open("/home/imyashkale/rustprojects/rust-in-action/src/storeroom/sample.txt").unwrap();
+    let mut reader = BufReader::new(f);
 
-    let b = Box::new(20);
-    println!("Heap known as a boxed integer : {:?}",b);
-
-    let c = Rc::new(Box::new(30));
-    println!("Boxed integer wrapped within a ref counter : {:?}", c);
-
-    let d = Arc::new(Mutex::new(40));
-    println!("Intger wrapped in an atomic ref counter & protected by a mutual exclusion lock {:?} ", d);
-}
-
-
-fn greet_world(){
-    println!("Greet World Function Execution started...");
-    // Assignment in Rust
-    // More properly called variable binding.
-    let southern_germany = "Grüß Gott!";
-    let japan = "ハロー・ワールド";
-    let regions = [ southern_germany, japan];
-    // Many type can have iter() to return the iterator
-    for region in regions.iter() {
-        // & amperdand "borrows" region for the read only access
-        println!("{}",&region)
+    let mut line = String::new();
+    loop {
+        let len = reader.read_line(&mut line).unwrap();
+        if len == 0 {
+            break;
+        }
+        println!("{} ({} bytes long)",line,len);
+        line.truncate(0);
     }
 }
 
-fn process_csv(){
-    let penguin_data = "/
-    common name, length (cm)
-    Little penguin,33
-    Yello-eyed penguin,65
-    Fiordland penguin,60
-    Invalid,data
-    ";
-
-    let records = penguin_data.lines();
-    for (index, record) in records.enumerate(){
-        if index == 0 || record.trim().len() == 0 {
-            println!("Iteration Skipped because of the whitespace");
-            continue;
-        }
-        // Vec<_> shorthand for vector
-        // can expand dynamically
-        //  (_) instructs rust to infer type of the elements.
-        let fields:Vec<_> = record
-            .split(',')
-            .map(|f | f.trim())
-            .collect();
-        // checks the configuration at the compile time.
-        if cfg!(debug_assertions) {
-            // eprintln1 prints to the standard error (stderr)
-            eprintln!("debug: {:?} -> {:?}", record,fields)
-        }
-        // NOTE :
-        // macros are similar to functions except that instead if returning data,
-        // these return code. often used to simplify common pattern
-        let name = fields[0];
-        if let Ok(l) = fields[1].parse::<f32>(){ // attempt to parse field as a floating point number
-            // The placeholder {} tekks rust to use a programmer method  to represent the value as string
-            // rather than the default representation available with {:?}
-            println!("Name :{}, Length : {}", name,l); // this prints to the std out
-        }
-        if index == 2 {
-            break
+// grep
+pub fn grep() {
+    let arg = Arg::with_name("pattern")
+        .help("The pattern to search for")
+        .takes_value(true)
+        .required(true);
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("Searched for patterns")
+        .arg(arg)
+        .get_matches();
+    let pattern = args.value_of("pattern").unwrap();
+    println!("Pattern : {}", pattern);
+    let re = Regex::new(pattern).unwrap();
+    let quote = "\
+        Every face, every shop, bedroom window, public-house, and
+        dark square is a picture feverishly turned--in search of what?
+        It is the same with books.
+        What do we seek through millions of pages?";
+    for line in quote.lines() {
+        let contains_substring = re.find(line);
+        match contains_substring {
+            Some(_) => println!("{}", line),
+            None => (),
         }
     }
 }
 
+
+pub fn intro_vec() {
+    let ctx_lines = 2;
+    let needle = "oo";
+    let haystack = "\
+    Every face, every shop,
+    bedroom window, public-house, and
+    dark square is a picture
+    feverishly turned--in search of what?
+    It is the same with books.
+    What do we seek
+    through millions of pages?";
+    let mut tags: Vec<usize> = vec![];
+    let mut ctx: Vec<Vec<(usize, String)>> = vec![];
+
+    for (index, line) in haystack.lines().enumerate() {
+        if line.contains(needle) {
+            tags.push(index);
+
+            let v = Vec::with_capacity(2 * ctx_lines + 1);
+            ctx.push(v)
+        }
+    }
+    if tags.is_empty() {
+        return;
+    }
+
+    for (index, line) in haystack.lines().enumerate() {
+        for (j, tag) in tags.iter().enumerate() {
+            let lower_bound = tag.saturating_sub(ctx_lines);
+            let upper_bound = tag + ctx_lines;
+
+            if (index >= lower_bound) && (index <= upper_bound) {
+                let line_as_string = String::from(line);
+                let local_ctx = (index, line_as_string);
+                ctx[j].push(local_ctx)
+            }
+        }
+    }
+    for local_ctx in ctx.iter() {
+        for &(index, ref line) in local_ctx.iter() {
+            let line_num = index + 1;
+            println!("{}: {}", line_num, line)
+        }
+    }
+}
+
+pub fn slice() {
+    //slices can grow
+    // array have fixed length
+}
+
+pub fn practice_array() {
+    let one = [1, 2, 3];
+    let two: [u8; 3] = [1, 2, 3];
+    let blank1 = [0; 3];
+    let blank2: [u8; 3] = [0; 3];
+
+    let arrays = [one, two, blank1, blank2];
+
+    for item in &arrays {
+        print!("{:?}:", item);
+        for n in item.iter() {
+            print!("\t{} + 10 = {}", n, n + 10);
+        }
+        let mut sum = 0;
+        for l in 0..item.len() {
+            sum += item[l]
+        }
+        println!("\t({:?} = {})", item, sum)
+    }
+}
